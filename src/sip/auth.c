2ca2b5f16976e3950f4949e2fac3ee7926be0736
Christoph Huber <c.huber@commend.com>
2023-06-26 14:29:43 +0200

    sip: adopt change of httpauth_digest to sip (stale changed to bool)

--- src/sip/auth.c
+++ src/sip/auth.c
@@ -166,7 +166,7 @@ static bool auth_handler(const struct sip_hdr *hdr, const struct sip_msg *msg,
 			goto out;
 	}
 	else {
-		if (!pl_isset(&ch.stale) || pl_strcasecmp(&ch.stale, "true")) {
+		if (!ch.stale) {
 			err = EAUTH;
 			goto out;
 		}
